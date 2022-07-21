b566ecff1d1612804baffbaa6b9e3c5c65878a98
todelusion <todelusion@gmail.com>
2022-07-21 16:32:53 +0800

    f/js init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const productList = document.querySelector('.js-productList')
+const apiUrl = 'https://livejs-api.hexschool.io/api/livejs/v1/customer/todelusion'
+
+function init(){
+    console.log('test')
+
+}
+init()
